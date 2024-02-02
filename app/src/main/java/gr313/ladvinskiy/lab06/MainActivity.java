619928691e04a2b728c841e1f328f986892162c9
Ladvinskiy Dmitry <myka124718@gmail.com>
2024-02-02 14:30:44 +0300

    lab06

---
+++ app/src/main/java/gr313/ladvinskiy/lab06/MainActivity.java
@@ -0,0 +1,82 @@
+package gr313.ladvinskiy.lab06;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter <Note> adp;
+    int sel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        adp = new ArrayAdapter<Note>(this, android.R.layout.simple_list_item_1);
+        ListView lst = findViewById(R.id.list_notes);
+        lst.setAdapter(adp);
+        lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                sel = position;
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if(requestCode == 12345) {
+            if (data != null) {
+                int pos = data.getIntExtra("my note index", 1);
+                String title = data.getStringExtra("my note title");
+                String content = data.getStringExtra("my note content");
+                Note n = adp.getItem(pos);
+                n.title = title;
+                n.content = content;
+                adp.notifyDataSetChanged();
+            }
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public void on_new_click(View v){
+        Note n = new Note();
+        n.title = "New note";
+        n.content = "Some content";
+        adp.add(n);
+        int pos = adp.getPosition(n);
+        Intent i = new Intent(this, SecondActivity.class);
+        i.putExtra("my note index", pos);
+        i.putExtra("my note title", n.title);
+        i.putExtra("my note content", n.content);
+        startActivityForResult(i, 12345);
+    }
+    public void on_edit_click(View v){
+        Note n = adp.getItem(sel);
+        Intent i = new Intent(this, SecondActivity.class);
+        i.putExtra("my note index", sel);
+        i.putExtra("my note title", n.title);
+        i.putExtra("my note content", n.content);
+        startActivityForResult(i, 12345);
+
+    }
+    public void on_delete_click(View v){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Вы уверены?");
+        dialog.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                adp.remove(adp.getItem(sel));
+            }
+        });
+        dialog.setNegativeButton("Нет", null);
+        dialog.show();
+    }
+}
---
+++ app/src/main/java/gr313/ladvinskiy/lab06/Note.java
@@ -0,0 +1,10 @@
+package gr313.ladvinskiy.lab06;
+
+public class Note {
+    public String title;
+    public String content;
+
+    public String toString(){
+        return title;
+    }
+}
